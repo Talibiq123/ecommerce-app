12ef1edddc81fb1eb9ab26843d80636427ddc744
Talibiq123 <talibsaeed90@gmail.com>
2024-10-17 19:57:47 +0530

    filter

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -1,5 +1,6 @@
 import React, { useContext, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
+import { assets } from '../assets/assets';
 
 const Collection = () => {
 
@@ -11,7 +12,9 @@ const Collection = () => {
 
       {/* Filter Options */}
       <div className='min-w-60'>
-        <p className=' my-2 text-xl flex items-center cursor-pointer gap-2'>FILTERS</p>
+        <p className=' my-2 text-xl flex items-center cursor-pointer gap-2'>FILTERS
+        <img src={assets.dropdown_icon} className={` h-3 sm:hidden ${showFilter? ' rotate-90': '' }`} alt="drop-down" />
+        </p>
         {/* Categary Filter */}
         <div className={`border border-gray-300 pl-5 py-3 mt-6 ${showFilter? '': 'hidden'} sm:block `}>
           <p className=' mb-3 text-sm font-medium'>CATEGORIES</p>
@@ -24,6 +27,21 @@ const Collection = () => {
             </p>
             <p className=' flex gap-2'>
               <input className=' w-3' type="checkbox" value={'Kids'} />Kids
+            </p>
+          </div>
+        </div>
+        {/* Subcategory Filters */}
+        <div className={`border border-gray-300 pl-5 py-3 mt-6 my-5 ${showFilter? '': 'hidden'} sm:block `}>
+          <p className=' mb-3 text-sm font-medium'>TYPE</p>
+          <div className=' flex flex-col gap-2 text-sm font-light text-gray-700'>
+            <p className=' flex gap-2'>
+              <input className=' w-3' type="checkbox" value={'Topwear'} />Topwear
+            </p>
+            <p className=' flex gap-2'>
+              <input className=' w-3' type="checkbox" value={'Bottomwear'} />Bottomwear
+            </p>
+            <p className=' flex gap-2'>
+              <input className=' w-3' type="checkbox" value={'Winterwear'} />Winterwear
             </p>
           </div>
         </div>
