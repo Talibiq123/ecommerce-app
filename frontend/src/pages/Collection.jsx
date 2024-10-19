d33cc66e7b26922688fb1083df930687e6c06109
Talibiq123 <talibsaeed90@gmail.com>
2024-10-19 20:43:28 +0530

    category...

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -9,6 +9,16 @@ const Collection = () => {
   const { products } = useContext(ShopContext);
   const [showFilter, setShowFilter] = useState(false);
   const [filterProducts, setFilterProducts] = useState([]);
+  const [category, setCategory] = useState([]);
+  const [subCategory, setSubCategory] = useState([]);
+
+  const toggleCategory = (e) => {
+    if (category.includes(e.target.value)) {
+      setCategory(prev => prev.filter(item => item !== e.target.value))
+    } else {
+      setCategory(prev => [...prev, e.target.value])
+    }
+  }
 
   useEffect(() => {
     setFilterProducts(products);
@@ -27,7 +37,7 @@ const Collection = () => {
           <p className=' mb-3 text-sm font-medium'>CATEGORIES</p>
           <div className=' flex flex-col gap-2 text-sm font-light text-gray-700'>
             <p className=' flex gap-2'>
-              <input className=' w-3' type="checkbox" value={'Men'} />Men
+              <input className=' w-3' type="checkbox" value={'Men'}  />Men
             </p>
             <p className=' flex gap-2'>
               <input className=' w-3' type="checkbox" value={'Women'} />Women
