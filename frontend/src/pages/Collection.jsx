8f15296f15b564b044da952310ff0bce5701205b
Talibiq123 <talibsaeed90@gmail.com>
2024-10-20 21:51:28 +0530

    category...

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -24,6 +24,11 @@ const Collection = () => {
     setFilterProducts(products);
   }, [])
 
+  useEffect(() => {
+    console.log(category);
+    
+  }, [])
+
   return (
     <div className=' flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
 
@@ -37,13 +42,13 @@ const Collection = () => {
           <p className=' mb-3 text-sm font-medium'>CATEGORIES</p>
           <div className=' flex flex-col gap-2 text-sm font-light text-gray-700'>
             <p className=' flex gap-2'>
-              <input className=' w-3' type="checkbox" value={'Men'}  />Men
+              <input className=' w-3' type="checkbox" value={'Men'} onClick={toggleCategory}  />Men
             </p>
             <p className=' flex gap-2'>
-              <input className=' w-3' type="checkbox" value={'Women'} />Women
+              <input className=' w-3' type="checkbox" value={'Women'} onClick={toggleCategory} />Women
             </p>
             <p className=' flex gap-2'>
-              <input className=' w-3' type="checkbox" value={'Kids'} />Kids
+              <input className=' w-3' type="checkbox" value={'Kids'} onClick={toggleCategory} />Kids
             </p>
           </div>
         </div>
