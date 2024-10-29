5dafae2add4a072d2de24e7368fab36f4cc0f959
Talibiq123 <talibsaeed90@gmail.com>
2024-10-29 15:52:33 +0530

    Login-SignUp done...

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -34,7 +34,7 @@ const Navbar = () => {
       <div className='flex items-center gap-6'>
         <img onClick={() => setShowSearch(true)} src={assets.search_icon} className='w-5 cursor-pointer' alt="search icon" />
         <div className='group relative'>
-            <img src={assets.profile_icon} alt="profile_icon" className='w-5 cursor-pointer' />
+        <Link to='/login'><img src={assets.profile_icon} alt="profile_icon" className='w-5 cursor-pointer' /></Link>
             <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
                 <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
                     <p className='cursor-poiner hover: text-black'>My Profile</p>
@@ -44,7 +44,7 @@ const Navbar = () => {
             </div>
         </div>
         <Link to='/cart' className='relative'>
-            <img src={assets.cart_icon} alt="cart" className='w-5 min-w-5' />
+        <img src={assets.cart_icon} alt="cart" className='w-5 min-w-5' />
             <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[-8]'>{getCartCount()}</p>
         </Link>
         <img onClick={() => setVisible(true)} src={assets.menu_icon} alt="menu" className='w-5 cursor-pointer sm:hidden' />
